63d1ea262ae9dcd4de1ba9623abc94bede7183a6
krivas404 <snaiper04ek353@gmail.com>
2022-06-20 18:25:54 +0300

    add text and functions for menu

---
+++ menu_and_input/console_menu_text.py
@@ -0,0 +1,33 @@
+def main_menu():
+    print(
+        "Меню:\r"
+        "1 - выдача контейнеров/получение контейнеров\r"
+        "2 - вывод информации о контейнерах на руках курьеров\r"
+        "3 - вывод отчёта об отданных/принятых контейнерах за сутки по фио курьеров\r"
+        "4 - тест сканера и штрихкодов\r"
+        "Введите цифру пункта меню, и нажмите Enter\r"
+        "Из любого меню можно выйти назад, нажав Q\r"
+          )
+
+def submenu_1_give_take_containers():
+    print(
+        ""
+
+    )
+
+
+def submenu_2_info_about_handon_containers():
+    pass
+
+
+def submenu_3_report_about_containers_today():
+    """print menu for report about all give on/take back operations with containers"""
+    pass
+
+
+def submenu_4_testmenu():
+    '''print menu for test'''
+    print(
+        "Отсканируйте любой штрих-код, и на экране появится вся известная информация:\r"
+        "что отсканировано, и какие записи об этом коде лежат в памяти программы или в базе данных\r"
+    )
